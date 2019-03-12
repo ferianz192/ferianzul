c0abaf2638fa21441b153476c5a162550918a213
ferianz192 <ferianz192@gmail.com>
2019-03-12 17:11:18 +0700

    template

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('layout', function () {
+    return view('layouts.master');
+});
+Route::get('dashboard', function () {
+    return view('dashboard.index');
+});
+Route::get('/lihatdata', 'CategoryController@index')->name('lihatdata');
